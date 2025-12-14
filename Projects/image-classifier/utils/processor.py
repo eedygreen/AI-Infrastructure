48b784db5791e93b39ce3a7d64b9e9d43985fede
Isah Idris <idris.isah2@st.futminna.edu.ng>
2025-12-14 22:56:51 +0100

    imc: cleanup

--- Projects/image-classifier/utils/processor.py
+++ Projects/image-classifier/utils/processor.py
@@ -1,8 +1,9 @@
 
 from enum import Enum
+import os
 from rich.console import Console
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Optional, Dict
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from processor import(
@@ -23,20 +24,20 @@ class MODE(str, Enum):
     INFERENCE = "inference"
 
 def process_image(
-        image_path: Union[str, List[str]] = None,
+        image_path: str = None,
         mode: MODE = Optional[MODE.INFERENCE],
-        size: Optional[int] = None,
-        resize: Optional[int] = None,
+        size: Optional[int] = 256,
+        resize: Optional[int] = 224,
         mean: List[float] = [0.485, 0.485, 0.485],
         std: List[float] = [0.229, 0.224, 0.225],
         batch_size: int = 32,
         shuffle: bool = False
-    ) -> DataLoader:
+    ) -> Dict:
     """
         Preprocess images for training or inference.
 
         Args:
-            image_path: Path to image(s) or list of paths
+            image_path: Directory containing images.
 
             mode: 'train' for training preprocessing, 'inference' for inference preprocessing
             
@@ -54,6 +55,7 @@ def process_image(
         
         Returns:
             DataLoader: PyTorch DataLoader with processed images
+            class_to_idx: mapping of classes to indices
     """
     if mode == MODE.TRAINING:
         transforms = Compose([
@@ -75,28 +77,23 @@ def process_image(
         subdir = 'test'
     else:
         raise ValueError("Invalid mode. Choose either 'train' or 'inference'.")
-    
-    if isinstance(image_path, str):
-        base_path = Path(image_path) 
-        full_path = base_path / subdir
-        if full_path.exists and full_path.is_dir():
-            target_path = full_path
-        elif base_path.is_dir():
-            target_path = base_path
-        else:
-            raise ValueError("Invalid image_path.")
-        console.log(f"[blue]Processing images from directory: {target_path}[/blue]")
-        image_datasets = datasets.ImageFolder(target_path, transform=transforms)
-    else:
-        raise ValueError("image_path must be a string path to directory.")
 
+    image_path = Path(image_path)
+    if image_path.is_dir():
+        image_path = os.path.join(image_path, subdir)
+
+    console.log(f"[blue]Processing images from directory: {image_path}[/blue]")
+
+    image_datasets = datasets.ImageFolder(image_path, transform=transforms)
 
     if len(image_datasets) == 0:
-        raise ValueError(f"No images found in the {target_path}directory.")
+        raise ValueError(f"No images found in the {image_path} directory.")
+
+    class_to_idx = image_datasets.class_to_idx
 
     dataloader = DataLoader(
         image_datasets, 
         batch_size, 
-        shuffle
+        shuffle,
     )
-    return dataloader
+    return dataloader, class_to_idx
