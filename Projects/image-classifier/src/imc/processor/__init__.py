eb7e96e35b76ae13397cb4186548dae665563079
Isah Idris <idris.isah2@st.futminna.edu.ng>
2025-12-17 23:32:46 +0100

    moved imc root to project

---
+++ Projects/image-classifier/src/imc/processor/__init__.py
@@ -0,0 +1,31 @@
+
+"""
+Image preprocessing and augmentation transforms using PIL/Pillow.
+
+This package provides PyTorch-style image transforms without requiring PyTorch.
+"""
+
+from imc.processor.processor import (
+    Compose,
+    RandomRotation,
+    RandomResizedCrop,
+    RandomHorizontalFlip,
+    Resize,
+    CenterCrop,
+    ToTensor,
+    Normalize
+)
+
+__all__ = [
+    "Compose",
+    "RandomRotation",
+    "RandomResizedCrop",
+    "RandomHorizontalFlip",
+    "Resize",
+    "CenterCrop",
+    "ToTensor",
+    "Normalize"
+]
+
+__version__ = "0.1.0"
+__author__ = "eeydgreen"
