ed0ac8f2bb683e7323e2f49da8c5aaa805e1471a
Isah Idris <idris.isah2@st.futminna.edu.ng>
2026-07-06 14:32:42 +0100

    categorised

--- Projects/image-classifier/src/imc/__init__.py
+++
@@ -1,5 +0,0 @@
-from imc.command import app 
-
-__all__ = ["app"]
-
-__version__ = "0.0.1"
--- Projects/image-classifier/src/imc/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from imc.utils.helper import Checkpoint
-from imc.utils.processor import process_image
-from imc.utils.validate import run_validation
-from imc.utils.train import train_model
-from imc.utils.predict import batch_predict 
-
-__all__ = ["Checkpoint", "process_image", "run_validation", "train_model", "batch_predict"]
-
-__version__ = "0.0.1"
